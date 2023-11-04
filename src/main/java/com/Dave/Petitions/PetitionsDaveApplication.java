09976a5779df7b23a45e3d5bf177ba273d24b311
David Oliveira <39216174+Davemoliveira@users.noreply.github.com>
2023-11-04 22:26:49 +0000

    Add files via upload

---
+++ src/main/java/com/Dave/Petitions/PetitionsDaveApplication.java
@@ -0,0 +1,13 @@
+package com.Dave.Petitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetitionsDaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetitionsDaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Dave/Petitions/PetitionsDaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dave.Petitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetitionsDaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
